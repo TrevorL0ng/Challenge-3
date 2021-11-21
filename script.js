b5828e9e3604044926ea3b3b13a58676cc7b3a7b
Trevor L <trevorlong992@gmail.com>
2021-11-21 15:26:02 -0500

    Chugging along. Prompts are working as expected. Most of the bones are here. Choice storage will be next.

--- script.js
+++ script.js
@@ -16,7 +16,29 @@ var confirmSymbols;
 // Write password to the #password input
 function writePassword() {
   window.alert("Let's generate a password! I have a few questions for you...");
+
+  // First user input. Requesting desired password length.
+
   userInput = parseInt(prompt("How many characters would you like this password to be? Please choose a number netween 8 and 128"));
+
+    if (!userInput){
+      window.alert("Please enter a value.");
+      
+    }
+
+    else if (userInput < 8 || userInput > 128){
+      window.alert("Please try again with a valid number");
+    }
+
+    // Now requesting the specifics of what characters the user would like to be included.
+
+    else {
+      window.alert ("Great! Now let's decide what you want to be in your password!");
+      confirmLowerCase = window.confirm("Would you like to use lower case letters?");
+      confirmUpperCase = window.confirm("How about some upper case letters?");
+      confirmNumbers = window.confirm("Want to throw some numbers in there?");
+      confirmSymbols = window.confirm("And finally, do you want to use some symbols?");
+    }
 
   var password = generatePassword();
  
