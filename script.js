69097b7b8efef8b4adaada12b8ca667a7b5eecca
Trevor L <trevorlong992@gmail.com>
2021-11-21 14:49:22 -0500

    Now we are cooking with gas. More of the bones put in place. Starting to look like something.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Declaring main values we want from the user
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '1234567890';
 var symbols = '!@#$%^&*()-_+=?';
+// Begin user input
+var userInput;
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumbers;
+var confirmSymbols;
+
 
 // Write password to the #password input
 function writePassword() {
   window.alert("Let's generate a password! I have a few questions for you...");
+  userInput = parseInt(prompt("How many characters would you like this password to be? Please choose a number netween 8 and 128"));
 
   var password = generatePassword();
  
