65083bbebb3b90ecf237d708a465cfd9ddba980d
Trevor L <trevorlong992@gmail.com>
2021-11-21 19:42:18 -0500

    Final touched up version for visuals. Now just for the readme. Almost there.

--- script.js
+++ script.js
@@ -25,28 +25,28 @@ function writePassword() {
 
   pwLength = parseInt(prompt("How many characters would you like this password to be? Please choose a number netween 8 and 128"));
 
-    if (!pwLength){
-      window.alert("Please enter a value.");
+      if (!pwLength){
+        window.alert("Please enter a value.");
       
-    }
+      }
 
-    else if (pwLength < 8 || pwLength > 128){
-      window.alert("Please try again with a valid number");
-    }
+      else if (pwLength < 8 || pwLength > 128){
+        window.alert("Please try again with a valid number");
+      }
 
     // Now requesting the specifics of what characters the user would like to be included.
 
-    else {
-      window.alert ("Great! Now let's decide what you want to be in your password!");
+        else {
+          window.alert ("Great! Now let's decide what you want to be in your password!");
 
-      confirmLowerCase = window.confirm("Would you like to use lower case letters?");
+          confirmLowerCase = window.confirm("Would you like to use lower case letters?");
 
-      confirmUpperCase = window.confirm("How about some upper case letters?");
+          confirmUpperCase = window.confirm("How about some upper case letters?");
 
-      confirmNumbers = window.confirm("Want to throw some numbers in there?");
+          confirmNumbers = window.confirm("Want to throw some numbers in there?");
 
-      confirmSymbols = window.confirm("And finally, do you want to use some symbols?");
-    }
+          confirmSymbols = window.confirm("And finally, do you want to use some symbols?");
+        }
 
     // Main argument to take user input and convert to the information we want
 
@@ -79,55 +79,56 @@ function writePassword() {
 
     // Combinations of two chosen password criteria 
 
-    else if (confirmLowerCase && confirmUpperCase) {
-      userInput = lowerCase.concat(upperCase);
-    }
+      else if (confirmLowerCase && confirmUpperCase) {
+        userInput = lowerCase.concat(upperCase);
+      }
 
-    else if (confirmLowerCase && confirmNumbers) {
-      userInput = lowerCase.concat(numbers);
-    }
+      else if (confirmLowerCase && confirmNumbers) {
+        userInput = lowerCase.concat(numbers);
+      }
 
-    else if (confirmLowerCase && confirmSymbols) {
-      userInput = lowerCase.concat(symbols);
-    }
+      else if (confirmLowerCase && confirmSymbols) {
+        userInput = lowerCase.concat(symbols);
+      }
 
-    else if (confirmUpperCase && confirmNumbers) {
-      userInput = upperCase.concat(numbers);
-    }
+      else if (confirmUpperCase && confirmNumbers) {
+        userInput = upperCase.concat(numbers);
+      }
 
-    else if (confirmUpperCase && confirmSymbols) {
-      userInput = upperCase.concat(symbols);
-    }
+      else if (confirmUpperCase && confirmSymbols) {
+        userInput = upperCase.concat(symbols);
+      }
 
-    else if (confirmNumbers && confirmSymbols) {
-      userInput = numbers.concat(symbols);
-    }
+      else if (confirmNumbers && confirmSymbols) {
+        userInput = numbers.concat(symbols);
+      }
 
     // Only one chosen input 
 
-    else if (confirmLowerCase) {
-      userInput = lowerCase;
-    }
+        else if (confirmLowerCase) {
+          userInput = lowerCase;
+        }
 
-    else if (confirmUpperCase) {
-      userInput = upperCase;
-    }
+        else if (confirmUpperCase) {
+          userInput = upperCase;
+        }
 
-    else if (confirmNumbers) {
-      userInput = numbers;
-    }
+        else if (confirmNumbers) {
+          userInput = numbers;
+        }
 
-    else if (confirmSymbols) {
-      userInput = symbols;
-    }
-  
+        else if (confirmSymbols) {
+          userInput = symbols;
+        }
+      
     // Now for the random generation 
 
     // Variable named final will be used to hold the collective string as it is generated 
 
     var final = [];
 
-    // For loop will be used to generate each character of the generation. The push function will be used to store each item to the end of the string 
+    // For loop will be used to generate each character of the generation. 
+    // The push function will be used to store each item to the end of the string as it is generated
      
     for (var x = 0; x < pwLength; x++) {
       var finalUserInput = userInput[Math.floor(Math.random() * userInput.length)];
